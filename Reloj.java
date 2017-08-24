f61654ae649f91b58d597f1485ec2840f494b6f1
Israel Vergara <israel.vergara102824@gmail.com>
2017-08-24 01:05:21 -0500

    se agrega metodo de ticTac()

--- Reloj.java
+++ Reloj.java
@@ -2,11 +2,13 @@ public class Reloj
 {
     private int horas;
     private int minutos;
+    private int incremento;
     
     public Reloj(int horaInicial, int minInicial)
     {
         horas = horaInicial;
         minutos = minInicial;
+        incremento = 1;
     }
     
     public String dimeTuTiempo()
@@ -19,4 +21,22 @@ public class Reloj
         
         return cadResultado;
     }
-}
+    
+    void ticTac()
+    {
+               
+        if(minutos <= 59 && minutos >=0)
+        {
+            minutos += incremento;
+            if(minutos == 60)
+            {
+                minutos = 0;
+                horas += incremento;
+                if(horas == 24)
+                {
+                    horas = 0; 
+                }
+            }
+        }
+    }
+ }
