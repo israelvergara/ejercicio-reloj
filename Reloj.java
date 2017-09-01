d102ecc76d108fc1ecf909a87f167dbfd1b90f24
Israel Vergara <israel.vergara102824@gmail.com>
2017-09-01 01:07:38 -0500

    se agregan comentarios

--- Reloj.java
+++ Reloj.java
@@ -1,9 +1,18 @@
+/**
+ * clase reloj muestra la hora y cada vez que hace in tic tac suma minutos al reloj.
+ * @author Israel Vergara
+ */
 public class Reloj
 {
     private int horas;
     private int minutos;
     private int incremento;
     
+    /**
+     * Constructor que inicializa variables como horas, minutos e incremento
+     * @param horaInicial y minInicia las cuales contienen las horas y  minutos
+     * iniciales respectivamente.
+     */
     public Reloj(int horaInicial, int minInicial)
     {
         horas = horaInicial;
@@ -11,6 +20,10 @@ public class Reloj
         incremento = 1;
     }
     
+    /**
+     * metodo que indica el tiempo
+     * @return cadRerultado variable de tipo String que contine la hora
+     */
     public String dimeTuTiempo()
     {
         String cadResultado = "";
@@ -22,7 +35,10 @@ public class Reloj
         return cadResultado;
     }
     
-    void ticTac()
+    /**
+     * Metodo que incrementa en uno el valor de los minutos
+     */
+    public void ticTac()
     {
                
         if(minutos <= 59 && minutos >=0)
@@ -39,4 +55,4 @@ public class Reloj
             }
         }
     }
- }
+}
